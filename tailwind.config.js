28dc84f645051365d1fc168938bea183d01c695b
fsousaentre <85347233+fredsons@users.noreply.github.com>
2023-01-24 14:51:12 -0300

    atualiza dependencias

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.css"],
   theme: {
+    screens: {
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
