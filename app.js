0c51c17269f7c39e2b7384e88cf3ae44186bf2a3
fsousaentre <85347233+fredsons@users.noreply.github.com>
2023-01-28 14:21:37 -0300

    atualizacao

--- app.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector("[data-collapse-toggle]");
-const menu = document.getElementById("navbar-solid-bg");
-
-toggleButton.addEventListener("click", function () {
-  menu.classList.toggle("hidden");
-});
